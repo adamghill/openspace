ad2fb9bda49a9e05b39c2856bcb5a1ed8be9fbe3
Adam Hill <adamghill@gmail.com>
2012-10-30 21:10:38 -0400

    Simulate real-world use by adding delay to set/get on the callbackDb.

--- node.js/src/04-callbacks/db/callbackDb.js
+++ node.js/src/04-callbacks/db/callbackDb.js
@@ -2,22 +2,19 @@ var db = {};
 
 function get(key, callback) {
 	if (db[key]) {
-		return callback(null, db[key]);
+		setTimeout(function() {
+			return callback(null, db[key]);
+		}, 10);
 	} else {
 		return callback(new Error('Key is not valid.'));
 	}
 }
 
 function set(key, value, callback) {
-	if (db[key]) {
-		// Update value
+	setTimeout(function() {
 		db[key] = value;
 		return callback(null, key);
-	} else {
-		// Create
-		db[key] = value;
-		return callback(null, key);
-	}
+	}, 100);
 }
 
 exports.get = get;
