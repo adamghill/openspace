aa70b994fd641c4217bf22bcf1375579f20651e9
Adam Hill <adamghill@gmail.com>
2012-10-30 21:52:35 -0400

    Make promises work correctly. Maybe.

--- node.js/src/04-callbacks/promises.js
+++ node.js/src/04-callbacks/promises.js
@@ -2,18 +2,24 @@ var db = require('./db/promisesDb');
 
 // $ node install rsvp
 // https://github.com/tildeio/rsvp.js#basic-usage
-db.set('key1', 'value1')
-.then(db.set('key2', 'value2'))
-.then(db.set('key3', 'value3'));
-
 var str = '';
-db.get('key1').then(function(value) {
-  str += value + ' - ';
-  return db.get('key2');
+
+db.set('key1', 'value1').then(function(key) {
+	return db.set('key2', 'value2');
+}).then(function() {
+	return db.set('key3', 'value3');
+}).then(function() {
+	return db.get('key1');
 }).then(function(value) {
-  str += value + ' - ';
-  return db.get('key3');
+	str += value + ' - ';
+	
+	return db.get('key2');
 }).then(function(value) {
-  str += value + ' - ';
-  console.log(str);
+	str += value + ' - ';
+	
+	return db.get('key3');
+}).then(function(value) {
+	str += value + ' - ';
+	
+	console.log(str);
 });
