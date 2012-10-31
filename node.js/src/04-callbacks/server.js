450bea026c5591d88af9aa40357f425f78451942
Adam Hill <adamghill@gmail.com>
2012-10-30 21:52:43 -0400

    Re-arrange order.

--- node.js/src/04-callbacks/server.js
+++ node.js/src/04-callbacks/server.js
@@ -1,6 +1,6 @@
 console.log('$ node naive.js');
 console.log('$ node callbacks.js');
 console.log('$ node evented.js');
-console.log('$ node wrapper.js');
-console.log('$ node flow.js');
 console.log('$ node promises.js');
+console.log('$ node flow.js');
+console.log('$ node wrapper.js');
